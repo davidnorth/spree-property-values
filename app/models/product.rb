c4afea5ba0390a1bc9179df07582df4b178b10ec
David North <davidnorth@david-norths-macbook-pro-15.local>
2009-03-26 17:33:55 +0000

    remove overe models and use class_eval in PropertyValuesExtension instead

--- app/models/product.rb
+++
@@ -1,6 +0,0 @@
-require_dependency RAILS_ROOT + '/app/models/product'
-class Product < ActiveRecord::Base
-
-  accepts_nested_attributes_for :product_properties, :allow_destroy => true
-  
-end
--- property_values_extension.rb
+++ property_values_extension.rb
@@ -4,7 +4,7 @@
 class PropertyValuesExtension < Spree::Extension
   version "1.0"
   description "Modify product properties to allow selection from a list rather than free text entry"
-  url "http://www.github.com/davidnorth/property_values"
+  url "http://www.github.com/davidnorth/spree-property-values"
 
   # Please use property_values/config/routes.rb instead for extension routes.
 
@@ -13,6 +13,31 @@ class PropertyValuesExtension < Spree::Extension
   # end
   
   def activate
-    # admin.tabs.add "Property Values", "/admin/property_values", :after => "Layouts", :visibility => [:all]
+
+    Product.class_eval do
+      accepts_nested_attributes_for :product_properties, :allow_destroy => true
+    end
+
+    Property.class_eval do
+      has_many :property_values, :attributes => true
+      accepts_nested_attributes_for :property_values, :allow_destroy => true
+    end
+    
+    Admin::ProductPropertiesController.class_eval do
+
+      create.response do |wants|
+        wants.html { redirect_to collection_url }
+      end
+
+      def update_all
+        @product = parent_object
+        @product.product_properties_attributes = params[:product][:product_properties_attributes]
+        @product.save!
+        flash[:notice] = "Properties updated"
+        redirect_to collection_url
+      end
+
+    end
+
   end
 end
